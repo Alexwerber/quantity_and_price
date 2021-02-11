aaa51d4ac566778ddbc80a6210917a2780baae64
Alexey Khoroshev <alexwerber42@gmail.com>
2021-02-11 20:38:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/appinteractive/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.appinteractive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        EditText priceValue = (EditText) findViewById(R.id.priceValue);
+        String repositoryOfInt = priceValue.getText().toString();
+        int value = Integer.parseInt(repositoryOfInt);
+
+        displayPrice(quantity * value);
+    }
+
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        display(quantity);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+}
